7d7dd8ba4e68f253bbf4552994660a472622172e
root <root@localhost.localdomain>
2021-03-29 12:55:41 -0400

    press

---
+++ app/src/main/java/com/beteiserichmond/beteiseweather/Splash.kt
@@ -0,0 +1,11 @@
+package com.beteiserichmond.beteiseweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
